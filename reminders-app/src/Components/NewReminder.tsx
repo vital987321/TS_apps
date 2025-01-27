88071d811bcd90f7564bd49568e7fc83e318e1aa
Vitalii Velychko <vitaliivelychko1@gmail.com>
2025-01-27 12:14:27 +0100

    Added NewReminder form

--- reminders-app/src/Components/NewReminder.tsx
+++ reminders-app/src/Components/NewReminder.tsx
@@ -1,11 +1,24 @@
-import React, { JSX } from 'react';
+import React, { JSX, useState } from 'react';
 
-function NewReminder():JSX.Element {
+interface NewReminderProps{
+    onAddReminder:(title:string)=>void
+}
+
+function NewReminder({ onAddReminder }:NewReminderProps): JSX.Element {
+    const [title, setTitle] = useState('')
+    
+    const submitForm = (e: React.FormEvent) => {
+        e.preventDefault()
+        if (!title) return;
+        onAddReminder(title)
+        setTitle('')
+    }
+
     return (
-        <form >
+        <form onSubmit={submitForm} >
             <label htmlFor="title"></label>
-            <input id='title' type="text" />
-            <button type='submit'>Add reminder</button>
+            <input value={title} onChange={e=>setTitle(e.target.value)} id='title' type="text" className='form-control' />
+            <button type='submit' className='btn btn-primary rounded-pill my-3'>Add reminder</button>
         </form>
     );
 }
